f68bab306ea76f6e04d72c6cb1c0058123868809
Samuel Uyigue <Samuel.uyigue@gmail.com>
2023-04-22 08:41:57 -0500

    Initial Commit

---
+++ CustomBottomTabBar/CustomBottomTabBarApp.swift
@@ -0,0 +1,17 @@
+//
+//  CustomBottomTabBarApp.swift
+//  CustomBottomTabBar
+//
+//  Created by Osaretin Uyigue on 4/22/23.
+//
+
+import SwiftUI
+
+@main
+struct CustomBottomTabBarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
